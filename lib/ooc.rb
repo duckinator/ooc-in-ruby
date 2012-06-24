901c0b1c12dce58b515e010e007485d931daf0f3
Nick Markwell <nick@duckinator.net>
2012-06-24 02:44:18 -0400

    Add Nagaqueen#ast accessor. Add note about getting fn list for Nagaqueen class.

--- lib/ooc.rb
+++ lib/ooc.rb
@@ -5,7 +5,12 @@ dir = File.expand_path(File.dirname(__FILE__))
 require 'pp'
 require File.join(File.expand_path(File.dirname(__FILE__)), 'nagaqueen')
 
+# Once all the types in nagaqueen/boilerplate.h are handled properly,
+# the following will generate a list of functions to define for the Nagaqueen class:
+#   cat nagaqueen/boilerplate.h |grep -E "^[a-z]" | sed 's/ \* /\* /' | cut -d' ' -f2- | sed 's/^ //' | sed 's/VALUE self, //'
 class Nagaqueen
+  attr_accessor :ast
+
   # Insert insanity here.
 end
 
